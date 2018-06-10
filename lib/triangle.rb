57fc0c3c680704cdeacb672ba437c14af9a88083
jarretbryan <jarret.bryan@gmail.com>
2018-06-10 17:26:03 -0400

    built incomplete methods

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
   # write code here
+
+  class TriangleError < StandardError
+  # triangle error code
+  end
+
+  attr_accessor :length1, :length2, :length3, :kind
+
+
+  def initialize (length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+    @kind = nil
+  end
+
+  def check_if_triangle
+    
+  end
+
+  def kind
+    if length1 == length2 && length1 == length3
+      @kind = :equilateral
+    elsif length1 == length2 || length2 == length3 || length1 == length3
+      @kind = :isosceles
+    else
+      :scalene
+    end
+  end
+
 end
