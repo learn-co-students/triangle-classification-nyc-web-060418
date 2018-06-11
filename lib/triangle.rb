7f9061578ba54567852c1bace761b3e08f952bf5
jarretbryan <jarret.bryan@gmail.com>
2018-06-10 21:54:00 -0400

    solved methods

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,6 +3,7 @@ class Triangle
 
   class TriangleError < StandardError
   # triangle error code
+    puts "That isn't an actual triangle. Try again!"
   end
 
   attr_accessor :length1, :length2, :length3, :kind
@@ -15,18 +16,29 @@ class Triangle
     @kind = nil
   end
 
-  def check_if_triangle
-    
+  def real_triangle?
+    real_triangle_param = [(@length1 + @length2 > @length3), (@length1 + @length3 > @length2), (@length3 + @length2 > @length1)]
+
+    triangle_example = [@length1, @length2, @length3]
+
+    raise TriangleError if
+      triangle_example.include?(0) ||
+      real_triangle_param.include?(false)
+    #end
+    # triangle_example.any? do |length|
+    #   length <= 0
+    # # end
   end
 
   def kind
-    if length1 == length2 && length1 == length3
-      @kind = :equilateral
-    elsif length1 == length2 || length2 == length3 || length1 == length3
-      @kind = :isosceles
-    else
-      :scalene
-    end
+    real_triangle?
+      if length1 == length2 && length1 == length3
+        @kind = :equilateral
+      elsif length1 == length2 || length2 == length3 || length1 == length3
+        @kind = :isosceles
+      else
+        :scalene
+      end
   end
 
 end
